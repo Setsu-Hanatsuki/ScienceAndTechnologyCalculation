b8977678f9892b2c25c35d166032035172dda2f0
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-15 18:03:31 +0900

    見やすく

--- graph3d.py
+++ graph3d.py
@@ -13,21 +13,21 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Make the X, Y meshgrid.
-coords=np.linspace(-4,4,200)
-X,Y=np.meshgrid(coords,coords)
+coords = np.linspace(-4, 4, 200)
+X, Y = np.meshgrid(coords, coords)
 
 # Set the z axis limits so they aren't recalculated each frame.
 ax.set_zlim(-1, 1)
 
 #animation function
-phi = np.linspace(0, 180. / np.pi, 50)
+phi = np.linspace(0, 180./np.pi, 50)
 ims = []
 for i in range(50):
     at_z = phi[i]
-    Z = generate(X,Y,at_z)
-    im = ax.plot_wireframe(X, Y, Z, cmap="rainbow",rstride=1, cstride=1,linewidth=0.1)
+    Z = generate(X, Y, at_z)
+    im = ax.plot_wireframe(X, Y, Z, cmap="rainbow", rstride=1, cstride=1, linewidth=0.1)
     ims.append([im])   
     
 ani = animation.ArtistAnimation(fig, ims, interval=100)
-ani.save('test_wave.gif',writer="ffmpeg")
+ani.save('test_wave.gif', writer="ffmpeg")
 HTML(ani.to_html5_video())
