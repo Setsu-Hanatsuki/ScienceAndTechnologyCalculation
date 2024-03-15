99619f65a594f20d27dc623f0cf9e21326effc54
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-15 17:59:10 +0900

    見やすく

--- baysiannet.py
+++ baysiannet.py
@@ -5,22 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx 
 
-df=pd.read_csv("iris.csv")
+df = pd.read_csv("iris.csv")
 
-SM=from_pandas(df)
+SM = from_pandas(df)
 SM.remove_edges_below_threshold(0.5)
 plt.figure(figsize=(16,9))
 pos = nx.spring_layout(SM, k=60)
-edge_width=[]
-val=[]
-for u,v,d in SM.edges(data=True):
-    val.append([str(u),str(v),d['weight']])
+edge_width = []
+val = []
+for u, v, d in SM.edges(data=True):
+    val.append([str(u), str(v), d['weight']])
     edge_width.append(d['weight']*0.1)
 nx.draw_networkx_labels(SM, pos)
-nx.draw_networkx(SM,pos,node_size=4000,arrowsize=20,alpha=0.6,edge_color='b',width=edge_width)
+nx.draw_networkx(SM,pos,node_size=4000, arrowsize=20, alpha=0.6, edge_color='b', width=edge_width)
 plt.show()
 
-columns=["origin","learned","weight"]
-dfo=pd.DataFrame(val)
-dfo.columns=columns
-dfo.to_csv("beysenetwork4.csv",encoding="shift-jis",index=False)
+columns = ["origin","learned","weight"]
+dfo = pd.DataFrame(val)
+dfo.columns = columns
+dfo.to_csv("beysenetwork4.csv", encoding="shift-jis", index=False)
